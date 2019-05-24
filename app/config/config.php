ac3493b9d4835b77acd42df1300dfec1050ffc0e
Tomonori Ueno <t.uepon@gmail.com>
2019-05-24 23:09:24 +0900

    first commit

---
+++ app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+  // DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASS', '5691tomochin');
+  define('DB_NAME', 'shareposts');
+
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'http://localhost/shareposts');
+  // Site Name
+  define('SITENAME', 'SharePosts');
