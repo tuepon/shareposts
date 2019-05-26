b4499cb6d89358dae90151273f54c7aec1b5d93f
Tomonori Ueno <t.uepon@gmail.com>
2019-05-27 06:24:43 +0900

    Edit User.php for validation

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -9,6 +9,23 @@
       if($_SERVER['REQUEST_METHOD'] == 'POST'){
         // Process form
 
+        // Sanitize POST data
+        $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+        // Init data(copied from below)
+        $data = [
+          'name' => trim($_POST['name']),
+          'email' => trim($_POST['email']),
+          'password' => trim($_POST['password']),
+          'confirm_password' => trim($_POST['confirm_password']),
+          'name_err' => '',
+          'email_err' => '',
+          'password_err' => '',
+          'confirm_password_err' => ''
+        ];
+
+        // Validate Email
+        
       } else {
         // Init data
         $data = [
