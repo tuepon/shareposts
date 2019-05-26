40a914237967a2d9eb689cfa6216de48d44e728f
Tomonori Ueno <t.uepon@gmail.com>
2019-05-26 14:08:27 +0900

    Create navbar

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,11 +1,13 @@
 <?php
   class Pages extends Controller {
     public function __construct(){
+
     }
 
     public function index(){
       $data = [
-        'title' => 'TraversyMVC',
+        'title' => 'SharePosts',
+        'description' => 'Simple social network built on the TraversyMVC PHP framework',
       ];
       
       $this->view('pages/index', $data);
@@ -13,7 +15,8 @@
     
     public function about(){
       $data = [
-        'title' => 'About Us'
+        'title' => 'About Us',
+        'description' => 'App to share posts with other users',        
       ];
 
       $this->view('pages/about', $data);
